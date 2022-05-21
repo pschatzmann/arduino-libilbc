d137f23f205b8b24116b929f49ac745fc55d0a61
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-21 20:18:09 +0200

    Split up Arduino API into decoder and encoder class

--- src/iLBC.h
+++ src/iLBC.h
@@ -9,62 +9,104 @@ extern "C" {
 }
 
 enum EnumLBCFrameSize { ms20 = 20, ms30 = 30 };
+enum EnumLBCDecodeMode { MODE_BAD = 0, MODE_NORMAL = 1 };
 
 /**
- * @brief Internet Low Bitrate Codec (iLBC) which is expecting int16_t samples
+ * @brief Internet Low Bitrate Codec (iLBC) Encoder which is expecting int16_t samples
  * @author Phil Schatzmann
  */
-class iLBC {
+class iLBCEncode {
 public:
-  iLBC(EnumLBCFrameSize frameSize = ms30, bool useEnhancer = true) {
+  iLBCEncode(EnumLBCFrameSize frameSize = ms30) {
     this->frameSize = frameSize;
-    block = new float[frameSize * 80];
     initEncode(&g_enc_inst, frameSize);
-    initDecode(&g_dec_inst, frameSize, useEnhancer);
+  }
+  ~iLBCEncode(){
+    if (block!=nullptr) delete block;
   }
 
   /// Number of decoded samples
-  int getSamples() { return this->frameSize * 80; }
+  int getSamples() { return g_enc_inst.blockl; }
   /// number of encoded bytes
   int getEncodedBytes() { return g_enc_inst.no_of_bytes; }
 
-  /// Encodes framesize of samples
+  /// Encodes framesize of int16_t samples 
   int encode(int16_t *samples, uint8_t *data) {
-
-    // Convert to float representaion of voice signal.
-    for (int i = 0; i < g_enc_inst.blockl; i++) {
+    if (block==nullptr){
+      block = new float[getSamples()];
+    }
+    // Convert to float representaion of voice signal. Returns number of bytes
+    for (int i = 0; i < getSamples(); i++) {
       block[i] = samples[i];
     }
 
     iLBC_encode(data, block, &g_enc_inst);
-
     return g_enc_inst.no_of_bytes;
   }
 
-  /// Decodes framesize of samples
-  int decode(uint8_t *data, int16_t *samples, bool mode) {
+  /// Encodes framesize of float samples 
+  int encode(float *samples, uint8_t *data) {
+    iLBC_encode(data, samples, &g_enc_inst);
+    return g_enc_inst.no_of_bytes;
+  }
 
-    iLBC_decode(block, data, &g_dec_inst, mode);
+protected:
+  iLBC_Enc_Inst_t g_enc_inst;
+  EnumLBCFrameSize frameSize;
+  float *block=nullptr;
+};
 
-    // Validate PCM16
-    for (int i = 0; i < g_dec_inst.blockl; i++) {
-      float point;
+/**
+ * @brief Internet Low Bitrate Codec (iLBC) Decoder which is expecting int16_t samples
+ * @author Phil Schatzmann
+ */
+class iLBCDecode {
+public:
+  iLBCDecode(EnumLBCFrameSize frameSize = ms30, bool useEnhancer = true) {
+    this->frameSize = frameSize;
+    initDecode(&g_dec_inst, frameSize, useEnhancer);
+  }
 
+  ~iLBCDecode(){
+    if (block!=nullptr) delete block;
+  }
+
+  /// Number of decoded samples
+  int getSamples() { return g_dec_inst.blockl; }
+  /// number of encoded bytes
+  int getEncodedBytes() { return g_dec_inst.no_of_bytes; }
+
+
+  /// Decodes framesize of int16_t samples - returns number of bytes
+  int decode(uint8_t *data, int16_t *samples, EnumLBCDecodeMode mode=MODE_NORMAL) {
+    if (block==nullptr){
+      block = new float[getSamples()];
+    }
+
+    iLBC_decode(block, data, &g_dec_inst, (int)mode);
+
+    // convert to PCM16
+    float point = 0.0;
+    for (int i = 0; i < getSamples(); i++) {
       point = block[i];
       if (point < MIN_SAMPLE) {
         point = MIN_SAMPLE;
       } else if (point > MAX_SAMPLE) {
         point = MAX_SAMPLE;
       }
-
       samples[i] = point;
     }
 
-    return g_dec_inst.blockl * 2;
+    return g_dec_inst.blockl * sizeof(int16_t);
+  }
+
+  /// Decodes framesize of float samples
+  int decode(uint8_t *data,float *samples, EnumLBCDecodeMode mode=MODE_NORMAL) {
+    iLBC_decode(samples, data, &g_dec_inst, (int)mode);
+    return g_dec_inst.blockl * sizeof(float);
   }
 
 protected:
-  iLBC_Enc_Inst_t g_enc_inst;
   iLBC_Dec_Inst_t g_dec_inst;
   EnumLBCFrameSize frameSize;
   float *block=nullptr;
